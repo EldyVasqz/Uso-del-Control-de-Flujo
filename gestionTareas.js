c28bafb82ee44d750ef8d1c167bc3995a20888ad
EldyVasqz <eldyarely17@gmail.com>
2023-09-21 20:50:41 -0600

    Hice dos programas para gestionar tareas y otro para verificar edad

---
+++ gestionTareas.js
@@ -0,0 +1,18 @@
+let tareaDelDia = "estudio"; 
+
+switch(tareaDelDia) {
+    case "ejercicio":
+        console.log("Recuerda estirar antes y después de tu rutina.");
+        break;
+    case "estudio":
+        console.log("Toma descansos cortos cada 30 minutos.");
+        break;
+    case "trabajo":
+        console.log("Prioriza tus tareas y evita las distracciones.");
+        break;
+    case "descanso":
+        console.log("Lee un libro o escucha música para relajarte.");
+        break;
+    default:
+        console.log("Esa no es una tarea válida.");
+}
